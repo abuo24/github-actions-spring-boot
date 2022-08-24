c352b3debf3660e3a4cd168a47af34ec1867a6d3
abuo24 <mryediniofficial9924@gmail.com>
2022-08-24 09:38:53 -0400

    first commit :)

--- github-actions-spring-boot/src/main/java/com/example/githubactionsspringboot/GithubActionsSpringBootApplication.java
+++ github-actions-spring-boot/src/main/java/com/example/githubactionsspringboot/GithubActionsSpringBootApplication.java
@@ -2,12 +2,22 @@ package com.example.githubactionsspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionsSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubActionsSpringBootApplication.class, args);
+    }
+
+
+    @GetMapping("/api/hello")
+    public ResponseEntity<?> hello(){
+        return ResponseEntity.ok("hello");
     }
 
 }
